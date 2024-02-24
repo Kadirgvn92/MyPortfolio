80eee3a98ad2d047f72db23a217373333255559e
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-02-25 01:14:12 +0300

    Admin layout created and some sections created

---
+++ MyPortfolio/Controllers/AboutController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.Models;
+
+namespace MyPortfolio.Controllers;
+public class AboutController : Controller
+{
+    private readonly DbMyPortfolioContext _dbMyPortfolioContext;
+
+    public AboutController(DbMyPortfolioContext dbMyPortfolioContext)
+    {
+        _dbMyPortfolioContext = dbMyPortfolioContext;
+    }
+
+    public IActionResult Index()
+    {
+        var values = _dbMyPortfolioContext.TblAbouts.ToList();
+        return View(values);
+    }
+}
---
+++ MyPortfolio/Controllers/FeatureController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.Models;
+
+namespace MyPortfolio.Controllers;
+public class FeatureController : Controller
+{
+    private readonly DbMyPortfolioContext _context;
+
+    public FeatureController(DbMyPortfolioContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var values = _context.TblFeatures.ToList();
+        return View(values);
+    }
+}
