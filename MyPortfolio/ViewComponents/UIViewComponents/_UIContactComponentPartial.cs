54e0213c7f1de56af543262688245be11e8d68d4
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-02-25 00:47:52 +0300

    Some UI components added

---
+++ MyPortfolio/ViewComponents/UIViewComponents/_UIContactComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolio.ViewComponents.UIViewComponents;
+
+public class _UIContactComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
--- MyPortfolio/ViewComponents/UIViewComponents/_UITestimonialComponentPartial.cs
+++ MyPortfolio/ViewComponents/UIViewComponents/_UITestimonialComponentPartial.cs
@@ -1,21 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyPortfolio.Models;
-using System.ComponentModel;
 
 namespace MyPortfolio.ViewComponents.UIViewComponents;
 
-public class _UIFeatureComponentPartial : ViewComponent
+public class _UITestimonialComponentPartial : ViewComponent
 {
     private readonly DbMyPortfolioContext _context;
 
-    public _UIFeatureComponentPartial(DbMyPortfolioContext context)
+    public _UITestimonialComponentPartial(DbMyPortfolioContext context)
     {
         _context = context;
     }
 
     public IViewComponentResult Invoke()
     {
-        var values = _context.TblFeatures.FirstOrDefault();
+        var values = _context.TblTestimonials.ToList();
         return View(values);
     }
 }
