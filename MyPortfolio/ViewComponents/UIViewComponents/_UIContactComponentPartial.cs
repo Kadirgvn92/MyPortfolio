38a62891f6be993ad7ceae2e387dcca12f9d5081
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-03-01 15:31:07 +0300

    All arrangments handled

--- MyPortfolio/ViewComponents/UIViewComponents/_UIContactComponentPartial.cs
+++ MyPortfolio/ViewComponents/UIViewComponents/_UIContactComponentPartial.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.Models;
 
 namespace MyPortfolio.ViewComponents.UIViewComponents;
 
 public class _UIContactComponentPartial : ViewComponent
 {
+    private readonly DbMyPortfolioContext _dbMyPortfolioContext;
+
+    public _UIContactComponentPartial(DbMyPortfolioContext dbMyPortfolioContext)
+    {
+        _dbMyPortfolioContext = dbMyPortfolioContext;
+    }
     public IViewComponentResult Invoke()
     {
         return View();
